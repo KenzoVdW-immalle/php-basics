9e78456da643c41ab2b9ca50da4490c5e809ebc9
KenzoVdW-immalle <kenzo.vandewinckel@immalle.be>
2017-02-20 09:27:43 +0100

    oef 05

--- 04_constants.php
+++ 04_constants.php
@@ -1,8 +1,11 @@
 <?php
 
 define("USERNAME", "joske");
-
+define("PASSWORD", "Imma");
+define("SERVERNAME", "Immalle");
 echo USERNAME;
+echo PASSWORD;
+
 
 // definieer ook een constante PASSWORD en SERVERNAME
 
