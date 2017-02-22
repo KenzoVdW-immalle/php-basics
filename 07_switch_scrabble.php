e65555b6a3b974260e326250cc3a78b01ca0fa55
kenzoVdW-immalle <kenzo.vandewinckel@immalle.be>
2017-02-22 16:06:18 +0100

    commit thuis

--- 07_switch_scrabble.php
+++ 07_switch_scrabble.php
@@ -5,7 +5,38 @@ $punten = 0;
 
 // gebruik een switch-case-statement binnen een for-loop om
 // te itereren over een woord en de Scrabble-punten ervan te berekenen
+for ($index=0; $index < strlen($woord) ; $index++) {
+switch ($woord[$index]) {
+  case 'q':
+    $punten = $punten + 10;
+    break;
+    case 'u':
+    $punten = $punten + 4;
+    break;
+    case 'o':
+    $punten = $punten + 1;
+    break;
+    case 't':
+    $punten = $punten + 2;
+    break;
+    case 'e':
+    $punten = $punten + 1;
+    break;
+    case 'r':
+    $punten = $punten +2;
+    break;
+    case 'i';
+    $punten = $punten + 1;
+    break;
+    case 'n':
+    $punten = $punten + 1;
+    break;
 
+  default:
+    # code...
+    break;
+}
+}
 // Q 10 punten
 // U 4 punten
 // O 1 punt
@@ -15,5 +46,5 @@ $punten = 0;
 // I 1 punt
 // N 1 punt
 
-
+echo $punten
 ?>
--- 09_superglobals.php
+++ 09_superglobals.php
@@ -1,15 +1,29 @@
+<?php
 // definieer een globale variable en print hem af op 2 manieren
+$x = 75;
+$y = 25;
 
+function addition() {
+    $GLOBALS['z'] = $GLOBALS['x'] + $GLOBALS['y'];
+}
+
+addition();
+echo $z;
+$Var = "hello";
+
+// inside a function
+function write() {
+    echo $GLOBALS['Var'];
+}
+write();
 
 
 
 // gebruik de $_SERVER-superglobal om zoveel mogelijk informatie af te drukken over het binnenkomende request
-
+echo $_SERVER['REQUEST_METHOD'];
 
 
 // Gebruik de $_GET-superglobal om de parameters van een URL in een ongenummerde lijst weer te geven.
 // Gebruik als URL b.v. `http://localhost/08_superglobals.php?kleur=rood&vorm=ovaal&grootte=medium`.
 // Tip: $_GET is een associatieve array.
-
-
-
+?>
